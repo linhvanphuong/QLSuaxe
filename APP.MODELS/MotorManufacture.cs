99520f3bc6ff6f78a7b841b15c3a8d7b37672ffe
Van Linh <linhvpbv@gmail.com>
2020-10-20 19:51:50 +0700

    commit

--- APP.MODELS/MotorManufacture.cs
+++ APP.MODELS/MotorManufacture.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace APP.MODELS
 {
-    class MotorManufacture
+    [Table("MotorManufacture")]
+    public class MotorManufacture
     {
+        [Column("Id")]
+        [Key]
+        public long Id { get; set; }
+        [Column("Name")]
+        public string Name { get; set; }
+        [Column("Status")]
+        public byte? Status { get; set; }
     }
 }
